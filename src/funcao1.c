683c087d0dd34d652f09c6f7da11d12cd4657743
Isabelle Cristina <isabarbosa1380@gmail.com>
2026-06-02 09:31:10 -0300

    Add: Arquivos de função

---
+++ src/funcao1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cabecalho(){
+
+    printf("\n====================================\n");
+    printf("\n========= Programa de Soma =========\n");
+    printf("\n====================================\n");
+    return 0;
+
+}
+
+int main(){
+
+    system("clear");
+    // vamos chamar uma função chamada cabecalho
+    cabecalho();
+    return 0;
+
+}
---
+++ src/funcao2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "../lib/funclayout.h"
+#include "../lib/funcsoma.h"
+
+int main(){
+    int x, y, z;
+
+    cabecalho();
+
+    printf("Digite um número inteiro e tecle ENTER\n");
+    scanf("%d",&x);
+    printf("Digite outro número inteiro e tecle ENTER\n");
+    scanf("%d",&y);
+    z = soma (x,y);
+    printf("O resultado da soma é %d\n",z);
+    rodape();
+    return 0;    
+
+}
